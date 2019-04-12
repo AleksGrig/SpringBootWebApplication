0b4074091903ad7905cdc92f59a86ed13ccb773c
Aleksey Grigoruk <ullitap@gmail.com>
2019-04-12 15:36:07 +0300

    HTML form added.

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
@@ -3,17 +3,24 @@ package com.in28minutes.springboot.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // /login => "Hello world"
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	// @ResponseBody
-	public String loginMessage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
+	public String showLoginPage(ModelMap model) {
 		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	// @ResponseBody
+	public String showWelcomePage(ModelMap model, @RequestParam String name) {
+		model.put("name", name);
+		return "welcome";
 	}
 
 }
