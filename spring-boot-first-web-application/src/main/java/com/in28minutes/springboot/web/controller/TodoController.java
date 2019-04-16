a809603b39004f6c8c47702a0528638a929c5e9c
Aleksey Grigoruk <ullitap@gmail.com>
2019-04-16 14:23:32 +0300

    Target Date fo todo, initBinder

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
@@ -1,13 +1,17 @@
 package com.in28minutes.springboot.web.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +27,14 @@ public class TodoController {
 	// Injected automatically
 	@Autowired
 	TodoService service;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		// dd/mm/yyyy
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, false));
+	}
 
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	// @ResponseBody
@@ -68,7 +80,7 @@ public class TodoController {
 			return "todo";
 		}
 
-		service.addTodo((String) model.get("name"), todo.getDesc(), new Date(), false);
+		service.addTodo((String) model.get("name"), todo.getDesc(), todo.getTargetDate(), false);
 		return "redirect:/list-todos";
 	}
 }
